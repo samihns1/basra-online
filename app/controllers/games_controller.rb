f5819b5a5e65315de6cc583bd29b0c57731a7dc8
samihns1 <samihs@uchicago.edu>
2025-11-23 18:40:46 +0000

    Changes to Create Game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,6 @@
 class GamesController < ApplicationController
   def index
     matching_games = Game.all
-
     @list_of_games = matching_games.order({ :created_at => :desc })
 
     render({ :template => "game_templates/index" })
@@ -9,59 +8,45 @@ class GamesController < ApplicationController
 
   def show
     the_id = params.fetch("path_id")
-
     matching_games = Game.where({ :id => the_id })
-
     @the_game = matching_games.at(0)
 
     render({ :template => "game_templates/show" })
   end
 
-def create
-  if current_user.nil?
-    redirect_to("/new_user", { :alert => "You must have an account to start a game." })
-    return
-  end
-
-  @game = Game.new
-
-  @game.creator = current_user
-  @game.status  = "waiting"
-  @game.started_at = Time.current
-
-  # Initialize game state
-  @game.deck_state  = initial_deck.to_json    # you’ll write initial_deck later
-  @game.table_cards = [].to_json
-  # winning_user_id, ended_at, gameplayers_count can stay nil/0 for now
-  
-    if the_game.valid?
-      the_game.save
-      redirect_to("/games", { :notice => "Game created successfully." })
-    else
-      redirect_to("/games", { :alert => the_game.errors.full_messages.to_sentence })
+  def create
+    if current_user.nil?
+      redirect_to("/", { :alert => "You must have an account to start a game." })
+      return
     end
-  end
 
-  private
+    @game = Game.new
 
-  def initial_deck
-    suits = %w[H D C S] # Hearts, Diamonds, Clubs, Spades
-    ranks = %w[A 2 3 4 5 6 7 8 9 10 J Q K]
+    @game.creator_id  = current_user.id 
+    @game.status      = "waiting"
+    @game.started_at  = Time.current
+    @game.table_cards = [].to_json
+    @game.deck_state  = initial_deck.to_json
 
-    suits.product(ranks).map { |suit, rank| "#{rank}#{suit}" }.shuffle
+    if @game.valid?
+      @game.save
+      redirect_to("/games/#{@game.id}", { :notice => "Game created successfully." })
+    else
+      redirect_to("/games", { :alert => @game.errors.full_messages.to_sentence })
+    end
   end
 
   def update
     the_id = params.fetch("path_id")
     the_game = Game.where({ :id => the_id }).at(0)
 
-    the_game.status = params.fetch("query_status")
-    the_game.creator_id = params.fetch("query_creator_id")
-    the_game.table_cards = params.fetch("query_table_cards")
-    the_game.deck_state = params.fetch("query_deck_state")
-    the_game.winning_user_id = params.fetch("query_winning_user_id")
-    the_game.started_at = params.fetch("query_started_at")
-    the_game.ended_at = params.fetch("query_ended_at")
+    the_game.status            = params.fetch("query_status")
+    the_game.creator_id        = params.fetch("query_creator_id")
+    the_game.table_cards       = params.fetch("query_table_cards")
+    the_game.deck_state        = params.fetch("query_deck_state")
+    the_game.winning_user_id   = params.fetch("query_winning_user_id")
+    the_game.started_at        = params.fetch("query_started_at")
+    the_game.ended_at          = params.fetch("query_ended_at")
     the_game.gameplayers_count = params.fetch("query_gameplayers_count")
 
     if the_game.valid?
@@ -79,5 +64,14 @@ def create
     the_game.destroy
 
     redirect_to("/games", { :notice => "Game deleted successfully." } )
+  end
+
+  private
+
+  def initial_deck
+    suits = %w[H D C S]
+    ranks = %w[A 2 3 4 5 6 7 8 9 10 J Q K]
+
+    suits.product(ranks).map { |suit, rank| "#{rank}#{suit}" }.shuffle
   end
 end
